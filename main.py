c982b232ffc318bfefca851e7d61284a0175ab97
Karim Nadim <karim_ossama94@hotmail.com>
2025-09-05 11:49:56 -0400

    Model evaluation pipeline completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_02_data_transformation import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.stage_03_model_trainer import ModelTrainerPipeline
+from src.textSummarizer.pipeline.stage_04_model_evaluation import ModelEvaluationPipeline
 from src.textSummarizer.logging import logger
 
 
@@ -31,6 +32,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    model_trainer = ModelTrainerPipeline()
    model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_evaluation = ModelEvaluationPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/textSummarizer/pipeline/stage_04_model_evaluation.py
@@ -0,0 +1,9 @@
+from src.textSummarizer.components.model_evaluation import ModelEvaluation
+
+class ModelEvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        model_evaluation_config = ModelEvaluation()
+        model_evaluation_config.evaluate_()
